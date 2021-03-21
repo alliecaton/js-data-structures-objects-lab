3241d5e4cbe1481ebb68c85ee63347452d15239f
Allie <alliecaton3@gmail.com>
2021-03-21 11:55:48 -0400

    commit tree cc0073fcfa77b5c6b38a0f76ee5245dc8ed9a31c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+let driver = {name: 'Sam'}
+
+function updateDriverWithKeyAndValue(driver, key, value) {    
+    let object = {...driver}
+    object[key] = value
+    return object
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value
+    return driver
+}
+
+function deleteFromDriverByKey(driver, key){
+    let newdriver = {...driver}
+    delete newdriver[key]
+    return newdriver
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key]
+    return driver
+}
